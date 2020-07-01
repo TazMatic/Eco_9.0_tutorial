781cf991bd32a89b5a83b87208fc701029ae7760
taylor <ymctaz@yahoo.com>
2020-07-01 10:58:07 -0400

    Start event listeners

--- Currency/Currency.cs
+++ Currency/Currency.cs
@@ -1,8 +1,36 @@
 ﻿using System;
+using Eco.Core.Controller;
+using Eco.Core.Plugins.Interfaces;
+using Eco.Core.Utils;
+using Eco.Core.Utils.Threading;
+using Eco.Gameplay;
+using Eco.Gameplay.GameActions;
+using Eco.Gameplay.Objects;
+using Eco.Gameplay.Systems;
+using Eco.Gameplay.Utils;
+using Eco.Shared.Items;
+using Eco.Shared.Localization;
+using Eco.Shared.Utils;
+using Eco.Shared.View;
 
 namespace Eco_9._0_tutorial
 {
-    public class Currency
+    public class Currency : IModKitPlugin, IServerPlugin
     {
+        private Boolean started = false;
+
+        public string GetStatus()
+        {
+            if (!started)
+            {
+                start();
+                started = true;
+            }
+            return "";
+        }
+        public void start()
+        {
+            //Do stuff here
+        }
     }
 }
---
+++ Events/Events.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Eco.Core.Plugins.Interfaces;
+using Eco.Core.Utils;
+using Eco.Gameplay.GameActions;
+
+
+namespace Eco_9._0_tutorial
+{
+    public class Currency : IModKitPlugin, IServerPlugin
+    {
+        private Boolean started = false;
+
+        public string GetStatus()
+        {
+            if (!started)
+            {
+                start();
+                started = true;
+            }
+            return "";
+        }
+        public void start()
+        {
+            var test1 = new Listen();
+            ActionUtil.AddListener(test1);
+        }
+    }
+    public class Listen : IGameActionAware
+    {
+        public void ActionPerformed(GameAction action)
+        {
+            Console.WriteLine("action: " + action.MarkedUpName + "\n type: " + action.GetType());
+            if (action.GetType() == typeof(DropOrPickupBlock))
+            {
+                Console.WriteLine("Match");
+                DropOrPickupBlock tmp = (DropOrPickupBlock)action;
+                //returns the block name
+                Console.WriteLine("Block: " + tmp.ItemUsed.DisplayName);
+                //return location of the block placed
+                Console.WriteLine("Location: " + tmp.Location);
+                //returns the username that placed the block
+                Console.WriteLine("Username: " + tmp.Citizen.Name);
+            }
+        }
+
+        public Result ShouldOverrideAuth(GameAction action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
